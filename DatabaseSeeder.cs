381f89f16e425ab324202712c7fd73b0da11054b
Lord-Velion <61228826+Lord-Velion@users.noreply.github.com>
2026-03-26 15:15:17 +0500

    Create DatabaseSeeder

---
+++ DatabaseSeeder.cs
@@ -0,0 +1,72 @@
+﻿using EcommerceSqlSolutions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceSqlSolutions
+{
+    public static class DatabaseSeeder
+    {
+        public static void SeedAgents(EcommerceContext context)
+        {
+            if (!context.Agents.Any())
+            {
+                for (int i = 1; i <= 15; i++)
+                {
+                    context.Agents.Add(new Agent { Id = i, Name = $"Agent{i}" });
+                }
+                context.SaveChanges();
+            }
+            else
+            {
+                for (int i = 1; i <= 15; i++)
+                {
+                    var agent = context.Agents.Find(i);
+                    Console.WriteLine($"Id: {agent.Id}, Name: {agent.Name}");
+                }
+            }
+        }
+
+        public static void SeedGoods(EcommerceContext context)
+        {
+            if (!context.Goods.Any())
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    context.Goods.Add(new Good { Id = i, Name = $"Good{i}" });
+                }
+                context.SaveChanges();
+            }
+            else
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    var good = context.Goods.Find(i);
+                    Console.WriteLine($"Id: {good.Id}, Name: {good.Name}");
+                }
+            }
+        }
+
+        public static void SeedColors(EcommerceContext context)
+        {
+            if (!context.Colors.Any())
+            {
+                for (int i = 1; i <= 3; i++)
+                {
+                    context.Colors.Add(new Color { Id = i, Name = $"Color{i}" });
+                }
+                context.SaveChanges();
+            }
+            else
+            {
+                for (int i = 1; i <= 3; i++)
+                {
+                    var color = context.Colors.Find(i);
+                    Console.WriteLine($"Id: {color.Id}, Name: {color.Name}");
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using EcommerceSqlSolutions;
 using EcommerceSqlSolutions.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,72 +10,8 @@ using (var context = new EcommerceContext())
 {
     context.Database.EnsureCreated();
 
-    // check if there are agents in the database
-    // if not then execute code to add 15 agents
-    if (!context.Agents.Any())
-    {
-        for (int i = 1; i <= 15; i++)
-        {
-            context.Agents.Add(new Agent
-            {
-                Id = i,
-                Name = $"Agent{i}"
-            });
-        }
-
-        context.SaveChanges();
-    }
-    else
-    {
-        for (int i = 1; i <= 15; i++)
-        {
-            Agent agent = context.Agents.Find(i);
-            Console.WriteLine($"Id: {agent.Id}, Name: {agent.Name}");
-        }
-    }
-
-    if (!context.Goods.Any())
-    {
-        for (int i = 1; i <= 10; i++)
-        {
-            context.Goods.Add(new Good
-            {
-                Id = i,
-                Name = $"Good{i}"
-            });
-        }
-
-        context.SaveChanges();
-    } 
-    else
-    {
-        for (int i = 1; i <= 10; i++)
-        {
-            Good good = context.Goods.Find(i);
-            Console.WriteLine($"Id: {good.Id}, Name: {good.Name}");
-        }
-    }
-
-    if (!context.Colors.Any())
-    {
-        for (int i = 1; i <= 3; i++)
-        {
-            context.Colors.Add(new Color
-            {
-                Id = i,
-                Name = $"Color{i}"
-            });
-        }
-
-        context.SaveChanges();
-    }
-    else
-    {
-        for (int i = 1; i <= 3; i++)
-        {
-            Color color = context.Colors.Find(i);
-            Console.WriteLine($"Id: {color.Id}, Name: {color.Name}");
-        }
-    }
+    DatabaseSeeder.SeedAgents(context);
+    DatabaseSeeder.SeedGoods(context);
+    DatabaseSeeder.SeedColors(context);
 }
 
