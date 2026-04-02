27540280f70bc224b9b620f5435dbecbdeb55a36
Lord-Velion <61228826+Lord-Velion@users.noreply.github.com>
2026-04-02 16:42:38 +0500

    Add indexes

--- Migrations/20260402113337_AddIndexes.Designer.cs
+++ Migrations/20260402113337_AddIndexes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EcommerceSqlSolutions.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EcommerceSqlSolutions.Migrations
 {
     [DbContext(typeof(EcommerceContext))]
-    partial class EcommerceContextModelSnapshot : ModelSnapshot
+    [Migration("20260402113337_AddIndexes")]
+    partial class AddIndexes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.13");
